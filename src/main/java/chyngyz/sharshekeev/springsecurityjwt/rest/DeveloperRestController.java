22aabba52df50df58c0bd46d7ca50ca55395da7b
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2021-11-19 12:27:10 +0600

    Initial commit

---
+++ src/main/java/chyngyz/sharshekeev/springsecurityjwt/rest/DeveloperRestController.java
@@ -0,0 +1,35 @@
+package chyngyz.sharshekeev.springsecurityjwt.rest;
+
+import chyngyz.sharshekeev.springsecurityjwt.model.Developer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestController {
+
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Asan", "Asanov"),
+            new Developer(2L, "Usen", "Usenov"),
+            new Developer(3L, "Marat", "Maratov")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll(){
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id){
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+}
---
+++ src/test/java/chyngyz/sharshekeev/springsecurityjwt/SpringSecurityJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package chyngyz.sharshekeev.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
